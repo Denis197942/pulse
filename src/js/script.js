b03e70caa5b409b0b79c3092858da739c1900ff4
Denis <kokotov.denis@yandex.ru>
2022-07-18 13:30:46 +0700

    post

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.carousel__inner').slick({
         infinite: true,
         slidesToShow: 1,
@@ -57,4 +57,25 @@ $(document).ready(function(){
           });
         })
         $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+        $('form').submit(function(e) {
+          e.preventDefault();
+
+          // if (!$(this).valid()) {
+          //   return;
+          // }
+
+          $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+          }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+          });
+          return false;
+        });
+
   });
